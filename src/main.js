06afcbd40b8574b504b355659dab6e8cc76dbb0d
xiaomizhou <xiaowu@qq.com>
2020-04-04 20:12:06 +0800

    项目起步-Element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import Vue from 'vue'
+/* 导入子组件 */
 import App from './App.vue'
+/* 导入插件 */
+import Vue from 'vue'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+/* 使用插件 */
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
